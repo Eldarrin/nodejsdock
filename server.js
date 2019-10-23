80e4f20c86c6f625f3111ee6f901c71c17e78e33
Eldarrin <32762846+eldarrin@users.noreply.github.com>
2019-10-23 11:02:32 +0100

    adding twistlock

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello to Hari and Jinesh\n');
+  res.send('Hello to UST Team\n');
 });
 
 app.listen(PORT, HOST);
